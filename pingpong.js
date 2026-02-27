86e91b73400a415a34481ebc76241f401ccf8c68
malusasdelli <maria.sasdelli.vieira@escola.pr.gov.br>
2026-02-27 07:57:06 -0300

    upde

--- pingpong.js
+++ pingpong.js
@@ -20,14 +20,33 @@ let yRaqueteOponente = 150;
 let meusPontos = 0;
 let pontosOponentes = 0; 
 
-function setup(){
-  createCanvas(600,400)
+// dificuldades 
+let dificuldade = "Normal";
+let trilha,ponto,raquetada;
+
+function preload(){
+  trilha = loadSound("trilha.mp3")
+  ponto = loadSound ("ponto.mp3")
+  raquetada = loadSound ("raquetada.mp3")
 }
 
-function draw(){
+function draw (){
+  background(0);
+  movimentoBolinha();
+  raquetes(xRaquete,yRaquete);
+  raquetes(xRaqueteOponente,yRaqueteOponente);
+  npc(dificuldades);
   
+  if(keyIsDown(UP_ARROW)){
+    yRaquete -= 10}
+  
+  if(KeyIsDown(DOWN_ARROW)){
+    yRaquete += 10}
+  
+  if(xBolinha -raio < xRaquete + 10 &
+    yBolinha-raio< yRaquete + 90 &
+    yBolinha +raio > yRaquete)
 }
-
 
 
 
@@ -39,3 +58,5 @@ function draw(){
 
 
 l
+
+
